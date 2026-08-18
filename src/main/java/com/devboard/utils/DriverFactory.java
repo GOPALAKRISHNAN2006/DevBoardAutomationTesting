40123a529c153dfe8fc516cd579fada386223591
unknown <gopalmuruga007@gmail.com>
2026-08-18 18:26:44 +0530

    Add a Github Action CI Workflow

--- src/main/java/com/devboard/utils/DriverFactory.java
+++ src/main/java/com/devboard/utils/DriverFactory.java
@@ -2,13 +2,25 @@ package com.devboard.utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
     public static WebDriver getDriver() {
 
+        ChromeOptions options =
+                new ChromeOptions();
+
+        // Run Chrome headless in GitHub Actions
+        if (System.getenv("GITHUB_ACTIONS") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+        }
+
         WebDriver driver =
-                new ChromeDriver();
+                new ChromeDriver(options);
 
         driver.manage().window().maximize();
 
